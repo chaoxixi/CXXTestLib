67cc7cbbff652dc42778a8f3f02092c3f0449238
xichao <402882461@qq.com>
2020-07-31 17:32:13 +0800

    .

---
+++ Example/Pods/Target Support Files/CXXTestLib/CXXTestLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CXXTestLib : NSObject
+@end
+@implementation PodsDummy_CXXTestLib
+@end
---
+++ Example/Pods/Target Support Files/Pods-CXXTestLib_Example/Pods-CXXTestLib_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CXXTestLib_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CXXTestLib_Example
+@end
