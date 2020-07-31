ecfd6152a984755f760a473c82b7db3b2a687434
xichao <402882461@qq.com>
2020-07-31 17:41:09 +0800

    …

---
+++ Example/CXXTestLib/tooleView.swift
@@ -0,0 +1,21 @@
+//
+//  tooleView.swift
+//  CXXTestLib_Example
+//
+//  Created by xichaogao on 2020/7/31.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class tooleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
